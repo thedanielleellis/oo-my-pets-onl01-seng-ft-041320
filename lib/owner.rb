1782a2428ebb51c987ccd8e91bddc3f0e1d10582
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-30 05:56:17 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,9 @@
 class Owner
   @@all []
-   attr_accessor :name, :species 
+   attr_accessor :name, :species
+   
+   def initialize(name,species)
+     @name = name 
    
   def buy_cat(name)
   end 
